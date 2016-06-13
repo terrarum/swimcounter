708b6d9e3e16fc3fa54c07e93400c39f75a0132f
terrarum <terrarum@gmail.com>
2016-06-13 14:53:09 +0100

    Correctly syncing

--- src/accel_logger.c
+++ src/accel_logger.c
@@ -1,26 +1,44 @@
 #include <pebble.h>
 #include "main.h"
 #include "accel_logger.h"
-#include "data_recorder.h"
-#include <@smallstoneapps/linked-list/linked-list.h>
 
 static char buf[60];
-uint32_t num_samples = 25;
-LinkedRoot* swim_data;
+const uint32_t num_samples = 20; // Seems to struggle to log more than 20 at a time.
+
+#define ACCEL_DATA_LOG 1
+static DataLoggingSessionRef s_session_ref;
+
+const int value = 16;
 
 // Handle the logged accelerometer data.
-void accel_data_handler(AccelData *data, uint32_t num_samples) {
-
-  // Store data in linked list.
-  for (uint32_t i = 0; i < num_samples; i++) {
+void accel_data_handler(AccelData *data, uint32_t num_sampless) {
+  
+  // Store data.
+  // Loops because I can't figure out how to batch-log data.
+  for (uint32_t i = 0; i < num_sampless; i++) {
+    DataLoggingResult result = data_logging_log(s_session_ref, &data[i], 1);
     
-//     APP_LOG(APP_LOG_LEVEL_INFO, "accel_data_handler data - t: %llu", data[i].timestamp);
+    switch((int)result) {
+      case DATA_LOGGING_SUCCESS:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "DATA_LOGGING_SUCCESS: %d", (int)result);
+        break;
+      case DATA_LOGGING_NOT_FOUND:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "DATA_LOGGING_NOT_FOUND: %d", (int)result);
+        break;
+      case DATA_LOGGING_CLOSED:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "DATA_LOGGING_CLOSED: %d", (int)result);
+        break;
+      case DATA_LOGGING_BUSY:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "DATA_LOGGING_BUSY: %d", (int)result);
+        break;
+      case DATA_LOGGING_INVALID_PARAMS:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "DATA_LOGGING_INVALID_PARAMS: %d", (int)result);
+        break;
+      case DATA_LOGGING_FULL:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "DATA_LOGGING_FULL: %d", (int)result);
+        break;
+    }
     
-    // Copy data.
-    AccelData *item = malloc(sizeof(AccelData));
-    memcpy(item, &data[i], sizeof(AccelData));
-    
-    linked_list_append(swim_data, item);
   }
 
   // Print to screen.
@@ -28,25 +46,10 @@ void accel_data_handler(AccelData *data, uint32_t num_samples) {
   text_layer_set_text(s_text_layer, buf);
 }
 
-// Linked list foreach callback.
-static bool process_swim_data(void* accelData, void* context) {
-  // Cast data back to AccelData.
-  AccelData* data = (AccelData*) accelData;
-  
-  int16_t x = data->x;
-  int16_t y = data->y;
-  int16_t z = data->z;
-  
-  APP_LOG(APP_LOG_LEVEL_INFO, "process_swim_data\nx: %d\ny: %d\nz: %d\n", x, y, z);
-  
-  return true;
-}
-
 // Start logging.
 void start_logging() {
-  APP_LOG(APP_LOG_LEVEL_INFO, "-----");
-  // Created linked list.
-  swim_data = linked_list_create_root();
+  // Beging DataLogging session.
+  s_session_ref = data_logging_create(ACCEL_DATA_LOG, DATA_LOGGING_BYTE_ARRAY, sizeof(AccelData), true);
   // Subscribe to the acceleration service.
   accel_data_service_subscribe(num_samples, accel_data_handler);
 }
@@ -55,6 +58,6 @@ void start_logging() {
 void stop_logging() {
   // Unsubscribe from acceleration service.
   accel_data_service_unsubscribe();
-  // Loop through the swim_data linked list.
-  linked_list_foreach(swim_data, process_swim_data, NULL);
+  // Finish logging data.
+  data_logging_finish(s_session_ref);
 }
--- src/data_recorder.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void set_data();
-void get_data();
